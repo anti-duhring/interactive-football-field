708829cf8114b2bbe0de7eed345c18b3fa2d641b
anti-duhring <99638905+anti-duhring@users.noreply.github.com>
2022-03-08 11:26:49 -0300

    Update dotplayers_info.js

--- script/dotplayers_info.js
+++ script/dotplayers_info.js
@@ -3,6 +3,7 @@ var playersAttr = {
     'cover1': 
     {
         'haveLineOnOffense': true,
+        //ID: [distancex, distancey, line, positionX, positionY, pairId]
         'FS': [20,20, 'line1', 200, 320, 'cover1_DM'],
         'DM': [100,120, 'line1', 0, 250, 'cover1_FS'],
         'CB1': [20,20, 'line2', 330, 40, 'WR1'],
@@ -26,6 +27,7 @@ var playersAttr = {
     'cover2man': 
     {   
         'haveLineOnOffense': true,
+        //ID: [distancex, distancey, line, positionX, positionY, pairId]
         'FS': [20,20, 'line1', 211, 259, 'cover2man_DL'],
         'DL': [130,120, 'line1', 0, 40, 'cover2man_FS'],
         'CB1': [20,20, 'line2', 330, 40, 'WR1'],
@@ -50,6 +52,7 @@ var playersAttr = {
     'cover2zone': 
     {
         'haveLineOnOffense': false,
+        //ID: [distancex, distancey, line, positionX, positionY, pairId]
         'FS': [20,20, 'line1', 211, 259, 'cover2zone_DL'],
         'DL': [130,120, 'line1', 0, 40, 'cover2zone_FS'],
         'CB1': [20,20, 'line2', 330, 40, 'cover2zone_LF'],
@@ -79,26 +82,62 @@ var playersAttr = {
     'cover3sky': 
     {
         'haveLineOnOffense': false,
-        'CB1': [20,20, 'line1', 330,40],
-        'DL': [100,120, 'line1', 0,30],
-        'CB2': [20,20, 'line2', 313, 166],
-        'LF': [70,90, 'line2', 245,0],
-        'SS': [20,20, 'line3', 286, 480],
-        'RF': [70,90, 'line3', 245, 520],
-        'RC': [70,90, 'line4', 245, 346],
-        'RLB': [20,20, 'line4', 309, 417],
-        'FS': [20,20, 'line5', 206, 327],
-        'DM': [100,120, 'line5', 0, 250],
-        'LLB': [20,20, 'line6', 308, 306],
-        'LC': [70,90, 'line6', 245, 173],
-        'CB3': [20,20, 'line7', 320, 579],
-        'DR': [100,120, 'line7', 0, 466],
+        //ID: [distancex, distancey, line, positionX, positionY, pairId]
+        'CB1': [20,20, 'line1', 330,40, 'cover3sky_DL'],
+        'DL': [100,120, 'line1', 0,30, 'cover3sky_CB1'],
+        'CB2': [20,20, 'line2', 313, 166, 'cover3sky_LF'],
+        'LF': [70,90, 'line2', 245,0, 'cover3sky_CB2'],
+        'SS': [20,20, 'line3', 286, 480, 'cover3sky_RF'],
+        'RF': [70,90, 'line3', 245, 520, 'cover3sky_SS'],
+        'RC': [70,90, 'line4', 245, 346, 'cover3sky_RLB'],
+        'RLB': [20,20, 'line4', 309, 417, 'cover3sky_RC'],
+        'FS': [20,20, 'line5', 206, 327, 'cover3sky_DM'],
+        'DM': [100,120, 'line5', 0, 250, 'cover3sky_FS'],
+        'LLB': [20,20, 'line6', 308, 306, 'cover3sky_LC'],
+        'LC': [70,90, 'line6', 245, 173, 'cover3sky_LLB'],
+        'CB3': [20,20, 'line7', 320, 579, 'cover3sky_DR'],
+        'DR': [100,120, 'line7', 0, 466, 'cover3sky_CB3'],
         'QB': [0,0, null, 300, 330, null],
         'RT': [0,0, null, 300, 330, null],
         'RG': [0,0, null, 300, 330, null],
         'C': [0,0, null, 300, 330, null],
         'LG': [0,0, null, 300, 330, null],
         'LT': [0,0, null, 300, 330, null],
+        'WR1': [20,20, null, null,null, null],
+        'WR2': [20,20, null, null,null, null],
+        'WR3': [20,20, null, null,null, null],
+        'TE': [20,20, null, null,null, null],
+        'RB': [20,20, null, null,null, null]
+    },
+    'cover4': 
+    {
+        'haveLineOnOffense': false,
+        //ID: [distancex, distancey, line, positionTOP, positionLEFT, pairId]
+        'FS': [20,20, 'line1', 211, 259, 'cover4_onefourth'],
+        'onefourth': [80,100, 'line1', 0, 173, 'cover4_FS'],
+        'CB1': [20,20, 'line6', 298, 92, 'cover4_twofourth'],
+        'LF': [90,110, 'line2', 215, 29, 'cover4_LLB'],
+        'CB3': [20,20, 'line4', 271, 531, 'cover4_threefourth'],
+        'RF': [90,110, 'line3', 215, 492, 'cover4_CB2'],
+        'threefourth': [80,100, 'line4', 0, 530, 'cover4_CB3'],
+        'CB2': [20,20, 'line3', 320, 490, 'cover4_RF'],
+        'RLB': [20,20, 'line5', 297, 401, 'cover4_H'],
+        'H': [90,110, 'line5', 167, 250, 'cover4_RLB'],
+        'LLB': [20,20, 'line2', 303, 218, 'cover4_LF'],
+        'twofourth': [80,100, 'line6', 0, 0, 'cover4_CB1'],
+        'SS': [20,20, 'line7', 243, 370, 'cover4_fourfourth'],
+        'fourfourth': [80,100, 'line7', 0, 354, 'cover4_SS'],
+        'QB': [0,0, null, 300, 330, null],
+        'RT': [0,0, null, 300, 330, null],
+        'RG': [0,0, null, 300, 330, null],
+        'C': [0,0, null, 300, 330, null],
+        'LG': [0,0, null, 300, 330, null],
+        'LT': [0,0, null, 300, 330, null],
+        'WR1': [20,20, null, null,null, null],
+        'WR2': [20,20, null, null,null, null],
+        'WR3': [20,20, null, null,null, null],
+        'TE': [20,20, null, null,null, null],
+        'RB': [20,20, null, null,null, null]
     }
 }
 
@@ -137,5 +176,14 @@ var linesAttr = {
         'line5': ['FS','DM','zone'],
         'line6': ['LLB','LC','zone'],
         'line7': ['DR', 'CB3','zone'],
+    },
+    'cover4': {
+        'line1': ['onefourth', 'FS','zone'],
+        'line2': ['LLB','LF','zone'],
+        'line3': ['CB2','RF','zone'],
+        'line4': ['CB3','threefourth','zone'],
+        'line5': ['RLB','H','zone'],
+        'line6': ['CB1','twofourth','zone'],
+        'line7': ['fourfourth', 'SS','zone'],
     }
 }
